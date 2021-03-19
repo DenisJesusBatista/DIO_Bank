4fe8803f807f4e773f86eaf0a78694a2ad5e054d
Denis Batista <denis.si2011@gmail.com>
2021-03-19 20:06:29 -0300

    Criando uma aplicação de transferências bancárias com .NET

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Denis de Jesus");
+            
+            Console.WriteLine(minhaConta.ToString());
+            Console.ReadLine();
+        }
+    }
+}
